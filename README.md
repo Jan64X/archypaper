bf02d5493230fe421d4b2479cbd31261d764d203
Jan Petrlík <petrlikhonzik@gmail.com>
2023-11-23 15:18:57 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# archypaper
-arch_paper thingy
+# 🐧 ArchPapers
+
+Some **Snazzy** [Wallpapers](img) for Arch Linux and a [CLI tool](CLI.md) to create your own!
+(You can use any logo, not just the Arch Linux one)
+
+### [🖼 Download Images](https://connorcode.com/files/Photos/ArchPapers.zip)
+
+### [🚀 Generate Your Own](CLI.md)
+
+<br>
+
+| [![](https://github.com/Basicprogrammer10/ArchPapers/assets/50306817/8016a87f-ca4a-46ed-88f0-6d6a9ca848aa)](img) | [![](https://github.com/Basicprogrammer10/ArchPapers/assets/50306817/659f44dd-c014-4ebc-8662-9bd82cf58c9f)](img) |
+| ---------------------------------------------------------------------------------------------------------------- | ---------------------------------------------------------------------------------------------------------------- |
+| [![](https://github.com/Basicprogrammer10/ArchPapers/assets/50306817/78871c89-8e38-4e00-91e1-4f63089b7e6e)](img) | [![](https://github.com/Basicprogrammer10/ArchPapers/assets/50306817/1e0db335-3035-4849-af8a-04c8c5b9db0c)](img) |
+
+<h3 align="center"><a href="img">[VIEW ALL]</a></h3>
---
+++ src/main.rs
@@ -0,0 +1,110 @@
+use std::{borrow::Cow, fs};
+
+use clap::Parser;
+use image::{DynamicImage, RgbaImage};
+use tiny_skia::{Color, Mask, MaskType, Pixmap, PremultipliedColorU8, Transform};
+use usvg::{Options, TreeParsing};
+
+mod args;
+
+const MASK: &[u8] = include_bytes!("../data/arch.svg");
+
+fn main() {
+    let arg = args::Args::parse();
+    println!("[*] Starting ArchPapers V{}", env!("CARGO_PKG_VERSION"));
+
+    println!("[*] Loading `{}`", arg.input.to_string_lossy());
+    let mut bg = match image::open(arg.input) {
+        Ok(i) => i,
+        Err(_) => return println!("[-] Invalid Image Input"),
+    };
+
+    // Translate
+    bg = DynamicImage::from(imageproc::geometric_transformations::translate(
+        &bg.to_rgb8(),
+        (arg.translate.0, arg.translate.1),
+    ));
+
+    // Blur
+    if let Some(i) = arg.blur {
+        println!("[*] Blurring Image");
+        bg = DynamicImage::from(imageproc::filter::gaussian_blur_f32(&bg.to_rgb8(), i));
+    }
+
+    // Darken
+    if let Some(i) = arg.darken {
+        println!("[*] Darking Image");
+        bg = bg.brighten(-i);
+    }
+
+    println!("[*] Generating Image");
+
+    // Load mask from SVG
+    let size = arg.size.unwrap_or((bg.width(), bg.height()));
+    let mask_data = match arg.mask {
+        Some(m) => Cow::Owned(fs::read(m).expect("Error reading mask")),
+        None => Cow::Borrowed(MASK),
+    };
+    let mask = render_mask(mask_data, size, arg.mask_scale, arg.invert);
+
+    // Create foreground mask
+    let mut colored = Pixmap::new(size.0, size.1).unwrap();
+    colored.fill(Color::from_rgba8(
+        arg.color[0],
+        arg.color[1],
+        arg.color[2],
+        255,
+    ));
+    colored.apply_mask(&mask);
+
+    // Scale background image
+    if arg.bg_scale != 1.0 {
+        bg = bg.resize(
+            (bg.width() as f32 * arg.bg_scale) as u32,
+            (bg.height() as f32 * arg.bg_scale) as u32,
+            image::imageops::FilterType::Lanczos3,
+        );
+    }
+
+    let overlay_x = (size.0 as i64 - bg.width() as i64) / 2;
+    let overlay_y = (size.1 as i64 - bg.height() as i64) / 2;
+
+    // Composite images
+    let mut image = image::RgbaImage::from_pixel(size.0, size.1, image::Rgba([0, 0, 0, 255]));
+    let colored_img = RgbaImage::from_raw(size.0, size.1, colored.data().to_vec()).unwrap();
+    image::imageops::overlay(&mut image, &bg, overlay_x, overlay_y);
+    image::imageops::overlay(&mut image, &colored_img, 0, 0);
+
+    println!("[*] Saving Image to `{}`", arg.output.to_string_lossy());
+    if let Err(e) = image.save(arg.output) {
+        println!("[-] Error saving image\n{e}");
+    };
+}
+
+fn render_mask(mask: Cow<[u8]>, size: (u32, u32), scale: f32, invert: bool) -> Mask {
+    let svg = usvg::Tree::from_data(&mask, &Options::default()).unwrap();
+    let svg = resvg::Tree::from_usvg(&svg);
+
+    let scale_x = size.0 as f32 / svg.size.width();
+    let scale_y = size.1 as f32 / svg.size.height();
+    let scale = scale_x.min(scale_y) * scale;
+
+    let translate_x = (size.0 as f32 - svg.size.width() * scale) / 2.0;
+    let translate_y = (size.1 as f32 - svg.size.height() * scale) / 2.0;
+
+    let mut pixmap = Pixmap::new(size.0, size.1).unwrap();
+    svg.render(
+        Transform::default()
+            .pre_scale(scale, scale)
+            .post_translate(translate_x, translate_y),
+        &mut pixmap.as_mut(),
+    );
+
+    if !invert {
+        pixmap.pixels_mut().iter_mut().for_each(|p| {
+            *p = PremultipliedColorU8::from_rgba(0, 0, 0, 255 - p.alpha()).unwrap();
+        });
+    }
+
+    Mask::from_pixmap(pixmap.as_ref(), MaskType::Alpha)
+}
